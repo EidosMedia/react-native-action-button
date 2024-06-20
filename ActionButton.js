5b0e8797dec4f401881121036c8d9fcc4f5abb96
Simone Corsato <simone.corsato@eidosmedia.com>
2024-06-20 16:13:01 +0200

    fixed ViewPropTypes has been removed from React Native

--- ActionButton.js
+++ ActionButton.js
@@ -7,7 +7,7 @@ import {
   Animated,
   TouchableOpacity
 } from "react-native";
-+import {TextPropTypes} from 'deprecated-react-native-prop-types';
+import {TextPropTypes} from 'deprecated-react-native-prop-types';
 import ActionButtonItem from "./ActionButtonItem";
 import {
   shadowStyle,
